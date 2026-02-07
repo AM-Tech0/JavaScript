d48fd1d87d4f9ef1187ad0034640b9eff22770b5
AM-Tech0 <amanbsdm6794@gmail.com>
2026-02-07 05:57:08 +0530

    first

---
+++ Basic1/index.js
@@ -0,0 +1,17 @@
+console.log(`Hello World! Lets Start JavaScript!`);
+// console.log(`This is my first JavaScript code!`);
+// let a=10;
+// console.log(a);
+// Javascript is a dynamic language,
+// Let is block scoped and var is function scoped.
+// var b=10;
+// if (b===10){
+// let num=10;
+// console.log(num);}
+// console.log(b); // This will work because b is declared with var and is function scoped.
+// console.log(num); // This will give an error because num is block scoped and cannot be accessed outside the if block.
+// Dyamaic typing
+let Name="Aman";
+console.log(Name);
+Name=30;
+console.log (Name);
